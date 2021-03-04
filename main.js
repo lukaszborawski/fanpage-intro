2bcdcfe739a1e287c111637ac4deb9e58d73e25c
Łukasz Borawski <lukasz.borawski@gmail.com>
2021-03-04 14:50:23 +0100

    add new img

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 const bgIntro = document.querySelector(".container_bgIntro");
-const mediaQuery = window.matchMedia("(max-width: 576px)");
-const numberOfImgs = 5;
+const mediaQuery = window.matchMedia("(orientation: portrait)");
+const numberOfImgs = 6;
 
+const clearBgIntro = (interval) => {
+    setTimeout(() => {
+        clearInterval(interval);
+        bgIntro.style.display = "none";
+    }, 7000)
+}
 
 const switchImage = () => {
+
     let imgNumber = 0;
-  
+
     if (mediaQuery.matches) {
         const interval = setInterval(() => {
             imgNumber = (imgNumber % numberOfImgs) + 1;
             bgIntro.style.backgroundImage = `url(img/${imgNumber}m.jpg)`
-        }, 850);
+        }, 800);
         
-        setTimeout(() => {
-            clearInterval(interval);
-            bgIntro.style.backgroundImage = "";
-        }, 9000)
+        clearBgIntro(interval);
+    
     } else {
         const interval = setInterval(() => {
             imgNumber = (imgNumber % numberOfImgs) + 1;
             bgIntro.style.backgroundImage = `url(img/${imgNumber}d.jpg)`
-        }, 850);
+        }, 800);
         
-        setTimeout(() => {
-            clearInterval(interval);
-            bgIntro.style.backgroundImage = "";
-        }, 9000); 
-    }
-    
+        clearBgIntro(interval);
+    } 
 }
 
 switchImage();
