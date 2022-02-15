351671581624482a1a49e209cb202af5ded4e52e
Łukasz Borawski <lukasz.borawski@gmail.com>
2022-02-15 18:30:40 +0100

    remove brackets

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 const bgIntro = document.querySelector(".container__bgIntro");
 const replay = document.querySelector(".replay__item");
 
-const clearBgIntro = (interval) => {
-    setTimeout(() => {
-        clearInterval(interval);
-        bgIntro.style.display = "none";
-    }, 9000)
+const clearBgIntro = interval => {
+  setTimeout(() => {
+    clearInterval(interval);
+    bgIntro.style.display = "none";
+  }, 9000)
 }
 
 const switchImage = () => {
 
-    const mediaQuery = window.matchMedia("(orientation: portrait)");
-    const numberOfImgs = 6;
-    let imgNumber = 0;
+  const mediaQuery = window.matchMedia("(orientation: portrait)");
+  const numberOfImgs = 6;
+  let imgNumber = 0;
 
-    if (mediaQuery.matches) {
-        const interval = setInterval(() => {
-            imgNumber = (imgNumber % numberOfImgs) + 1;
-            bgIntro.style.backgroundImage = `url(img/${imgNumber}m.jpg)`
-        }, 800);
+  if (mediaQuery.matches) {
+    const interval = setInterval(() => {
+      imgNumber = (imgNumber % numberOfImgs) + 1;
+      bgIntro.style.backgroundImage = `url(img/${imgNumber}m.jpg)`
+    }, 800);
 
-        clearBgIntro(interval);
+    clearBgIntro(interval);
 
-    } else {
-        const interval = setInterval(() => {
-            imgNumber = (imgNumber % numberOfImgs) + 1;
-            bgIntro.style.backgroundImage = `url(img/${imgNumber}d.jpg)`
-        }, 800);
+  } else {
+    const interval = setInterval(() => {
+      imgNumber = (imgNumber % numberOfImgs) + 1;
+      bgIntro.style.backgroundImage = `url(img/${imgNumber}d.jpg)`
+    }, 800);
 
-        clearBgIntro(interval);
-    }
+    clearBgIntro(interval);
+  }
 }
 
 switchImage();
